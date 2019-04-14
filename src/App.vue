61c106d7e876141d36ed0bbdc071d1dab4937660
Paul Barnes <paul.barnes.psnl@gmail.com>
2019-04-14 13:36:28 +0100

    create Map component, with Google Maps initialisation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map></Map>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from '@/components/Map'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Map
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body, html {
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+    padding: 0;
+  }
+
+  #app {
+    display: flex;
+    height: 100%;
+  }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+import googleMapsInit from '@/utils/googleMaps'
+
+export default {
+  async mounted () {
+    const google = await googleMapsInit()
+
+    // Init our map
+    // eslint-disable-next-line
+    const map = new google.maps.Map(this.$el, {
+      center: {
+        lat: 0,
+        lng: 0
+      },
+      zoom: 6
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+    #map {
+        flex: 0 0 75%;
+        max-width: 75%;
+    }
+</style>
